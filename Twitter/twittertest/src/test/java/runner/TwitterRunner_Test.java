5a55a37ffd89cbad9bea233b7e031a2ccf5598fc
SandipanBhattacharjee <sandipanbhattacharjee1981@gmail.com>
2019-06-16 08:30:19 -0500

    modifying the pom.xml and adding the reports options

--- Twitter/twittertest/src/test/java/runner/TwitterRunner_Test.java
+++ Twitter/twittertest/src/test/java/runner/TwitterRunner_Test.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "Features", 
 glue = {"glue"}, 
-tags = {" @getSubscribedLists or @getHomeTimeline"})
-public class Get_RelatedSuite_Test {
+plugin= {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+public class TwitterRunner_Test {
 
 }
+//tags = {" @getSubscribedLists or @getHomeTimeline"}
